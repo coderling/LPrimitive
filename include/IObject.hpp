3ecb9b2d67c0513558c8ca295dae7e0fac2f7d7b
coderling <coderling@gmail.com>
2022-10-12 14:39:03 +0800

    init commit

---
+++ include/IObject.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "CommonDefines.hpp"
+#include "InterfaceUUID.hpp"
+
+namespace Toy::Engine
+{
+template <typename OtherInterface>
+class RefCountWeakPtr;
+template <typename OtherInterface>
+class RefCountPtr;
+class ReferenceCounter;
+}  // namespace Toy::Engine
+
+namespace Toy
+{
+class IReferenceCounter;
+
+class IObject
+{
+   public:
+    virtual TOY_RESULT ENGINE_FUNCTION_CALL_CONVENTION QueryInterface(const IUUID& iid, IObject** pp_interface) = 0;
+    void Destroy() { OnDestroy(); }
+    virtual ~IObject() noexcept {}
+
+   protected:
+    template <typename OtherInterface>
+    friend class Toy::Engine::RefCountWeakPtr;
+    template <typename OtherInterface>
+    friend class Toy::Engine::RefCountPtr;
+    friend class Engine::ReferenceCounter;
+    virtual long AddReference() = 0;
+    virtual long Release() = 0;
+    virtual IReferenceCounter* GetReferenceCounter() = 0;
+    virtual void OnDestroy() = 0;
+};
+}  // namespace Toy
---
+++ include/LoadDll.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+
+namespace Toy::Engine
+{
+inline FARPROC LoadDLL(const char* dll_name, const char* function_name)
+{
+#if _WIN64
+    const char* arc = "_x64";
+#else
+    const char* arc = "_x86";
+#endif
+
+#if _DEBUG || ENGINE_DEBUG
+    const char* config = "_d";
+#else
+    const char* config = "_r";
+#endif
+
+    const size_t StringBufferSize = 4096;
+    char* lib_name = (char*)malloc(StringBufferSize);
+
+    sprintf_s(lib_name, StringBufferSize, "%s%s%s.dll", dll_name, arc, config);
+
+    auto handle = LoadLibraryA(lib_name);
+
+    if (handle == nullptr)
+        {
+            std::cerr << "failed to load dll: " << lib_name << std::endl;
+            return nullptr;
+        }
+
+    auto p_func = GetProcAddress(handle, function_name);
+    if (p_func == nullptr)
+        {
+            std::cerr << "failed to get function: " << function_name << std::endl;
+            FreeLibrary(handle);
+            return nullptr;
+        }
+
+    return p_func;
+}
+}  // namespace Toy::Engine
